9e04675aca5d2c37ce84dd4b28ae063ffc59d851
unknown <youssef.mhenni1@gmail.com>
2023-01-14 15:04:56 +0100

    Sidebar funct component

---
+++ src/Components/Sidebar-session/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./sidebar.scss"
+import logo from '../../Assets/logo-app.png'
+import {AiOutlineCalculator, AiOutlinePieChart} from "react-icons/ai";
+import {BsBatteryCharging,} from "react-icons/bs";
+import {BsChatDots,} from "react-icons/bs";
+import {BsFolderSymlink,} from "react-icons/bs";
+import {GiComputerFan,} from "react-icons/gi";
+import {TbToolsOff} from "react-icons/tb";
+
+
+const Sidebar = (props) => (
+    <div className={'sideBar'}>
+
+    </div>
+
+);
+
+export default Sidebar;
